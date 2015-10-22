45f0b590515d2da3592fdab0549a1f6abb13d683
Kevin Qu <quchunguang@gmail.com>
2015-10-22 18:04:03 +0800

    add some samples to standlone/

---
+++ standlone/testbluemonday/testbluemonday.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
+)
+
+func main() {
+	input := []byte(`# The Title Level 1
+This is a simple **common style** [markdown](https://daringfireball.net/projects/markdown/) text.
+
+* item1 - *OKOK*
+* item2 - [Google](http://google.com)
+* item3 - [Baidu](http://baidu.com)
+`)
+	// Generate HTML from Markdown
+	html := blackfriday.MarkdownCommon(input)
+
+	// Using UGCPolicy to filter HTML Generated
+	html = bluemonday.UGCPolicy().SanitizeBytes(html)
+	fmt.Println(string(html))
+
+	// Query HTML using jquery style goquery
+	buf := bytes.NewBuffer(html)
+	goquery.NewDocument()
+	query, err := goquery.NewDocumentFromReader(buf)
+	if err != nil {
+		panic(err)
+	}
+	query.Find("li a").Each(func(_ int, items *goquery.Selection) {
+		url, exists := items.Attr("href")
+		if exists {
+			fmt.Println(url)
+		}
+	})
+}
---
+++ standlone/testmiddlewarechan/testmiddlewarechan.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middlewareOne")
+		next.ServeHTTP(w, r)
+		log.Println("Executing middlewareOne again")
+	})
+}
+
+func middlewareTwo(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middlewareTwo")
+		if r.URL.Path != "/" {
+			return
+		}
+		next.ServeHTTP(w, r)
+		log.Println("Executing middlewareTwo again")
+	})
+}
+
+func final(w http.ResponseWriter, r *http.Request) {
+	log.Println("Executing finalHandler")
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	finalHandler := http.HandlerFunc(final)
+
+	http.Handle("/", middlewareOne(middlewareTwo(finalHandler)))
+	http.ListenAndServe(":3000", nil)
+}
