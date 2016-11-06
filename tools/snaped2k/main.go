699967cba5f7ef64a69c40760261994b0c3e2e5a
Kevin Qu <quchunguang@gmail.com>
2016-11-06 21:42:07 +0800

    change arguments

--- tools/snaped2k/main.go
+++ tools/snaped2k/main.go
@@ -10,20 +10,21 @@ import (
 )
 
 func main() {
-	var cn163 string
+	var argIds, argURL string
 	var ids []string
 
-	flag.StringVar(&cn163, "cn163", "", "Comma separated list of ids.")
+	flag.StringVar(&argIds, "ids", "", "Comma separated list of ids, like `11,31,51`. Each joined after the given `--url`.")
+	flag.StringVar(&argURL, "url", "cn163.net/archives/", "URL of the site without `http://`. `cn163.net/archives/` by default.")
 	flag.Parse()
 	if flag.NFlag() == 0 {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	ids = strings.Split(cn163, ",")
+	ids = strings.Split(argIds, ",")
 
 	for _, id := range ids {
-		link := "http://cn163.net/archives/" + id
+		link := "http://" + argURL + id
 		doc, err := goquery.NewDocument(link)
 		if err != nil {
 			fmt.Println("Cannot open url")
