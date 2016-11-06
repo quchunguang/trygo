274c414db35060ccc4d25114dcbf8e581607c398
Kevin Qu <quchunguang@gmail.com>
2016-11-06 17:18:37 +0800

    add command line arguments

--- tools/snaped2k/main.go
+++ tools/snaped2k/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"net/url"
@@ -9,19 +10,32 @@ import (
 )
 
 func main() {
-	doc, err := goquery.NewDocument("http://cn163.net/archives/3083/")
-	if err != nil {
-		fmt.Println("Cannot open url")
+	var cn163 string
+	var ids []string
+
+	flag.StringVar(&cn163, "cn163", "", "Comma separated list of ids.")
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	f, _ := os.Create("out.txt")
-	defer f.Close()
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		href := s.AttrOr("href", "")
-		if strings.HasPrefix(href, "ed2k://") {
-			url, _ := url.QueryUnescape(href)
-			f.WriteString(url + "\n")
+	ids = strings.Split(cn163, ",")
+
+	for _, id := range ids {
+		link := "http://cn163.net/archives/" + id
+		doc, err := goquery.NewDocument(link)
+		if err != nil {
+			fmt.Println("Cannot open url")
+			os.Exit(2)
 		}
-	})
+
+		doc.Find("a").Each(func(i int, s *goquery.Selection) {
+			href := s.AttrOr("href", "")
+			if strings.HasPrefix(href, "ed2k://") {
+				uu, _ := url.QueryUnescape(href)
+				fmt.Println(uu)
+			}
+		})
+	}
 }
