d7fe0dd1a038cd2155fc45372821ee87041775d1
Kevin Qu <quchunguang@gmail.com>
2017-11-15 20:04:44 +0800

    little

--- demos/demo_issues/main.go
+++ demos/demo_issues/main.go
@@ -1,53 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
-	"time"
+	"io"
+	"os"
 )
 
-// WRONG!!! For val variable in the above loops is actually a single variable.
-// func wrong1(values []int) {
-// 	for val := range values {
-// 		// go val.my_method()
-// 		go func() {
-// 			fmt.Print(val)
-// 		}()
-// 	}
-// }
-
-// RIGHT!!! The val is evaluated at each iteration and placed on the stack for the goroutine.
-func right1(values []int) {
-	for val := range values {
-		go func(val interface{}) {
-			fmt.Print(val)
-		}(val)
-	}
-}
-
-// RIGHT!!! The variables declared within the body of a loop are not shared between iterations, and thus can be used separately in a closure.
-func right2(values []int) {
-	for i := range values {
-		val := values[i]
-		go func() {
-			fmt.Print(val)
-		}()
-	}
-}
-
-// RIGHT!!! Without executing this closure as a goroutine, the code runs as expected
-func right3(values []int) {
-	for i := 0; i < 10; i++ {
-		func() {
-			fmt.Print(i)
-		}()
-	}
-}
 func main() {
-	values := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var b bytes.Buffer // A Buffer needs no initialization.
+	b.Write([]byte("Hello "))
+	fmt.Fprintf(&b, "world!\n")
+	b.WriteTo(os.Stdout)
 
-	// wrong1(values)
-	// right1(values)
-	// right2(values)
-	right3(values)
-	time.Sleep(time.Second)
+	// A Buffer can turn a string or a []byte into an io.Reader.
+	buf := bytes.NewBufferString("R29waGVycyBydWxlIQ==")
+	dec := base64.NewDecoder(base64.StdEncoding, buf)
+	io.Copy(os.Stdout, dec)
 }
